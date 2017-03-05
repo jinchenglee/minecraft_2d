153dcfb5fc5f64e2f43c778fd70d1576221a8044
jinchenglee <jincheng.li@gmail.com>
2017-03-05 13:53:56 -0800

    Add color map and tile map.

--- main.py
+++ main.py
@@ -4,11 +4,45 @@ import pygame, sys
 # Import some useful constants
 from pygame.locals import *
 
-# Initialize the pygame module
-pygame.init()
+# Constants representing colors
+BLACK = (0, 0, 0)
+BROWN = (153, 76, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
+# Constnats representing the different resources
+DIRT = 0
+GRASS = 1
+WATER = 2
+COAL = 3
+
+# A dictionary linking resources to colors
+colors = {
+            DIRT    : BROWN,
+            GRASS   : GREEN,
+            WATER   : BLUE,
+            COAL    : BLACK
+        }
+
+# Aa list representing our tilemap
+tilemap = [
+            [GRASS, COAL, DIRT],
+            [WATER, WATER, GRASS],
+            [COAL, GRASS, WATER],
+            [DIRT, GRASS, COAL],
+            [GRASS, WATER, DIRT]
+        ]
+
+# Useful game dimensions
+TILESIZE = 40
+MAPWIDTH = 3
+MAPHEIGHT = 5
+
+# Set up display
+pygame.init() 
 # Create a new drawing surface, width=?, height=?
-DISPLAYSURF = pygame.display.set_mode((300,300))
+DISPLAYSURF = pygame.display.set_mode((MAPWIDTH*TILESIZE,MAPHEIGHT*TILESIZE))
+
 # Give the window a caption
 pygame.display.set_caption("james's 2D World")
 
@@ -21,9 +55,12 @@ while True:
             # end the game and close the window
             pygame.quit()
             sys.exit()
-    pygame.draw.rect(DISPLAYSURF, (143,212,101), (231,111,232,82))
-    pygame.draw.rect(DISPLAYSURF, (243,0,0), (0,0,20,20))
-    pygame.draw.rect(DISPLAYSURF, (231,245,45), (99,67,67,100))
-    pygame.draw.rect(DISPLAYSURF, (243,129,255), (22,56,90,70))
+
+    # Loop through each row
+    for row in range(MAPHEIGHT):
+        # loop through each column in a row
+        for column in range(MAPWIDTH):
+            pygame.draw.rect(DISPLAYSURF, colors[tilemap[row][column]], (column*TILESIZE, row*TILESIZE, TILESIZE, TILESIZE))
+
     # update the display
     pygame.display.update()
