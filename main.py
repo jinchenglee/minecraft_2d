4ff9a9cc2590fa0890b8e5ab144f21d14da4fbb3
jinchenglee <jincheng.li@gmail.com>
2017-03-19 16:59:54 -0700

    Added inventory scoring.

--- main.py
+++ main.py
@@ -37,9 +37,21 @@ textures = {
             DIAMOND : pygame.image.load(resource_dir+'diamond.png')
         }
 
+# Inventory
+inventory = {
+            DIRT    : 0,
+            GRASS   : 0,
+            WATER   : 0,
+            COAL    : 0,
+            COTTON  : 0,
+            ROCK    : 0,
+            LAVA    : 0,
+            DIAMOND : 0 
+        }
+
 # Useful game dimensions
 TILESIZE = 40
-MAPWIDTH = 15
+MAPWIDTH = 35
 MAPHEIGHT = 20
 
 # Aa list representing our tilemap
@@ -47,8 +59,12 @@ tilemap = [ [ DIRT for w in range(MAPWIDTH)] for h in range(MAPHEIGHT)]
 
 # Set up display
 pygame.init() 
+
+# Add a font for our inventory
+INVFONT = pygame.font.Font('FreeSansBold.ttf', 18)
+
 # Create a new drawing surface, width=?, height=?
-DISPLAYSURF = pygame.display.set_mode((MAPWIDTH*TILESIZE,MAPHEIGHT*TILESIZE))
+DISPLAYSURF = pygame.display.set_mode((MAPWIDTH*TILESIZE,MAPHEIGHT*TILESIZE+60))
 # Change the ratio of different resources
 for rw in range(MAPHEIGHT):
     for cl in range(MAPWIDTH):
@@ -103,6 +119,11 @@ while True:
             elif (event.key==K_DOWN) and playerPos[1] < MAPHEIGHT-1:
                 playerPos[1] += 1
 
+            if event.key==K_SPACE:
+                currentTile = tilemap[playerPos[1]][playerPos[0]]
+                inventory[currentTile] += 1
+                tilemap[playerPos[1]][playerPos[0]] = DIRT
+
     # Loop through each row
     for row in range(MAPHEIGHT):
         # loop through each column in a row
@@ -111,6 +132,17 @@ while True:
 
     # Display the player
     DISPLAYSURF.blit(PLAYER, (playerPos[0]*TILESIZE, playerPos[1]*TILESIZE))
+
+    # Inventory display
+    placePosition = 10
+    for item in inventory:
+        # Draw the item image
+        DISPLAYSURF.blit(textures[item], (placePosition, MAPHEIGHT*TILESIZE+15))
+        placePosition += 30
+        # Add text show the amount of item in inventory
+        textObj = INVFONT.render(str(inventory[item]), True, WHITE, BLACK)
+        DISPLAYSURF.blit(textObj, (placePosition, MAPHEIGHT*TILESIZE+15))
+        placePosition += 50
     
     # update the display
     pygame.display.update()
